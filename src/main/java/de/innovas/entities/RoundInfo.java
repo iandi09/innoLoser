e128a2ec5333263eb09461db6618d12baebe5b76
Andre <andre.ehlert09@gmail.com>
2017-01-16 22:06:49 +0100

    Modified round length

--- src/main/java/de/innovas/entities/RoundInfo.java
+++ src/main/java/de/innovas/entities/RoundInfo.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 
 public class RoundInfo {
 	
-	public static int ROUND_LENGTH = 24;
+	public static int ROUND_LENGTH = 26;
 
 	@Id
 	private String id;
